077423e1529a74caeca7987b44ec665d18c82fe0
AO-Alumno <AO-Alumno@duoc.acad>
2026-03-25 20:05:29 -0300

    Initial commit

---
+++ src/test/java/com/evaluacionformativa/listatareas/ListaTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacionformativa.listatareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
